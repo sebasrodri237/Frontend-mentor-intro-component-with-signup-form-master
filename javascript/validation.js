a26410baed071f605f67e00707553110ae4ccc0b
sebasrodri237 <sebasplay2@hotmail.com>
2020-02-20 01:07:17 -0500

    Continue with the error state actives in Javascript, I found a simple solution later a long time

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,51 +1,30 @@
-const firstName = document.getElementById("first_name");
-const lastName = document.getElementById("last_name");
-const mail = document.getElementById("mail").placeholder;
-const password = document.getElementById("password");
-const buttonSend = document.getElementById("button_send");
+function getValues(){
 
-const form  = document.getElementsByTagName('form');
+    const firstName = document.getElementById("first_name");
+    const lastName = document.getElementById("last_name");
+    const mail = document.getElementById("mail");
+    const password = document.getElementById("password");
+    const buttonSend = document.getElementById("button_send");
 
-const mailError = document.getElementsByClassName("register-container__error");
+    const form  = document.getElementById("form");
 
-console.log(mail);
+    const error = document.getElementsByClassName("register-container__error");
+    form.addEventListener('submit',validationForm(error),false);
 
-// mail.addEventListener('input', function (event) {
-//   // Cada vez que el usuario escribe algo, verificamos si
-//   // los campos del formulario son válidos.
+}
+    
+function validationForm(error) {
 
-//   if (mail.validity.valid) {
-//     // En caso de que haya un mensaje de error visible, si el campo
-//     // es válido, eliminamos el mensaje de error.
-//     mailError.innerHTML = ''; // Restablece el contenido del mensaje
-//     mailError.className = 'register-container__error'; // Restablece el estado visual del mensaje
-//   } else {
-//     // Si todavía hay un error, muestra el error exacto
-//     showError();
-//   }
-// });
+    
+    if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(mail.value)){
+        console.log("La dirección de email " + mail.value + " es correcta.");
+        error[2].style.display = "none";
+       } else {
+           console.log("La dirección de email es invalida ");
+           error[2].style.display = "initial";
+    }
+  }
 
-// form.addEventListener('submit', function (event) {
-//     // si el campo de correo electrónico es válido, dejamos que el formulario se envíe
-  
-//     if(!mail.validity.valid) {
-//       // Si no es así, mostramos un mensaje de error apropiado
-//       showError();
-//       // Luego evitamos que se envíe el formulario cancelando el evento
-//       event.preventDefault();
-//     }
-//   });
-  
-//   function showError() {
-//     if(mail.validity.valueMissing) {
-//       // Si el campo está vacío
-//       // muestra el mensaje de error siguiente.
-//       mailError.textContent = 'Debe introducir una dirección de correo electrónico.';
-//     } else if(mail.validity.typeMismatch) {
-//       // Si el campo no contiene una dirección de correo electrónico
-//       // muestra el mensaje de error siguiente.
-//       mailError.textContent = 'El valor introducido debe ser una dirección de correo electrónico.';
-//     } 
-//     // Establece el estilo apropiado
-//     mailError.className = 'error activo';
-//   }
+
+
+
